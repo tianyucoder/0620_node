67cc51e4e0bb6be20824967bf40de4c326023736
zhangtianyu <zhangtianyu5177@sina.com>
2019-09-23 16:12:02 +0800

    init

--- day01/6.Buffer缓冲器/node_Buffer.js
+++ day01/6.Buffer缓冲器/node_Buffer.js
@@ -9,30 +9,35 @@
 * 2. 进制转化
 *       十六进制：00 ----- ff
 *       十进制：0 ----- 255
-*       二进制：0000000 ------ 11111111
+*       二进制：00000000 ------ 11111111
 *
 * 3.内存单位的换算
 *       8位(bit)  = 1字节(Byte)
 *       1024Byte = 1KB
-*       1024Kb = 1Mb
-*       1024Mb = 1Gb
+*       1024Kb = 1MB
+*       1024Mb = 1GB
 *       ...
 * */
 
 //1.使用new关键字创建一个Buffer实例，即将废弃，因为：1.效率极低；2.存在一些性能问题
-/*let buf = new Buffer(10)
-console.log(buf)*/
+let buf = new Buffer(10)
+console.log(buf)
 
 //2.使用Buffer.alloc创建一个Buffer实例，效率比new关键字稍高
-/*let buf2 = Buffer.alloc(10)
-console.log(buf2)*/
+let buf2 = Buffer.alloc(10)
+console.log(buf2)
 
-//3.Buffer.allocUnsafe创建一个Buffer实例
+//3.Buffer.allocUnsafe创建一个Buffer实例，效率最高的一种方式
 /*
 * 1.为什么通过Buffer.allocUnsafe创建出来的实例，里面不为空？
-* 2.为什么存的是二进制，输出的却不是二进制。
+* 2.为什么存的是二进制，输出的却不是二进制？ ---- 存储的确实为二进制数据，但是，一旦输出为了便于观察，自动转成16进制。
 * */
 let buf3 = Buffer.allocUnsafe(10)
 console.log(buf3)
+
+//4.将一个字符串保存在Buffer中
+let str = 'HELLO ATGUIGU'
+let buf4 = Buffer.from(str)
+console.log(buf4)
 
 
