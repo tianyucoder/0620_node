ef2fbed8703a209da5da946c17f7edd482edccc3
zhangtianyu <zhangtianyu5177@sina.com>
2019-09-24 17:00:04 +0800

    init

--- day02/1.Node中的文件操作(读取)/2.流式文件读取.js
+++ day02/1.Node中的文件操作(读取)/2.流式文件读取.js
@@ -15,7 +15,11 @@
 
 let fs = require('fs');
 
-let rs = fs.createReadStream(`${__dirname}/music.mp3`)
+let rs = fs.createReadStream(`${__dirname}/music.mp3`,{
+  highWaterMark:1024 * 1024 //控制每次读取文件的大小（水管的粗细），每次读取1MB
+})
+
+let ws = fs.createWriteStream(__dirname+'/music2.mp3')
 
 //只要用到了流，就要对流进行监听
 rs.on('open',function () {
@@ -24,13 +28,19 @@ rs.on('open',function () {
 
 rs.on('close',function () {
   console.log('可读流关闭了')
+  ws.end()
+})
+ws.on('open',function () {
+  console.log('可写流打开了')
+})
+
+ws.on('close',function () {
+  console.log('可写流关闭了')
 })
 
 //给可读流绑定一个data事件，会自动触发可读流读取数据
-rs.on('data',(err,data)=>{
-    if(!err){
-      console.log(data)
-    }else{
-      console.log(err)
-    }
+rs.on('data',(data)=>{
+    console.log('读出了一些东西')
+    ws.write(data)
+    //console.log(data.length)//Buffer的length是数据的大小
 })
---
+++ day02/4.mongoose的基本使用/1.mongoose连接数据库.js
@@ -0,0 +1,29 @@
+/*
+* mongoDB:一个数据库品牌的名字
+* mongod:启动数据库服务的命令
+* mongo:连接数据库的命令（用mongoDB自带的“黑窗口”去连接）
+* mongoose:一个优秀的库，用于在Node平台上，让我们更加简单、方便、稳定的连接和操作mongoDB数据库
+* */
+
+//1.引入mongoose模块
+const mongoose = require('mongoose')
+
+//2.使用mongoose连接数据库
+mongoose.connect('mongodb://localhost:27017/demo',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+//3.绑定监听
+mongoose.connection.on('open',(err)=>{
+    if(!err){
+      console.log('数据库连接成功了')
+      console.log('操作数据库的代码')
+    }else{
+      console.log(err)
+    }
+})
+
+
+
+
