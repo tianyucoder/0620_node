912fff649e1fe8a3aef20440fb13f232c9b1021b
zhangtianyu <zhangtianyu5177@sina.com>
2019-09-25 15:00:12 +0800

    init

--- day03/3.node原生服务器/server.js
+++ day03/3.node原生服务器/server.js
@@ -3,15 +3,20 @@
 * */
 
 //引入Node内置的Http模块
-let http = require('http');
+let http = require('http')
+//querystring专门用于解析查询字符串参数为一个对象
+let querystring = require('querystring')
 
 //搞一个服务员出来-------创建服务对象
 let server = http.createServer(function (request,response) {
     //请求对象：包含着客户端发给服务器的信息
     //响应对象：包含着服务器发给客户端的信息
-    console.log('有人请求服务器了')
+    let params = request.url.split('?')[1]
+    let obj = querystring.parse(params)
+    console.log(obj)
+    let {name,age} = obj
     response.setHeader('content-type','text/html;charset=utf-8')
-    response.end('<h1>第一个服务器，好开心</h1>')
+    response.end(`<h1>你的名字是：${name},你的年龄是：${age}</h1>`)
 })
 
 //指定端口运行
