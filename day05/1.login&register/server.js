a413dba11592c3876309287efe3ea97ebcf7bed3
zhangtianyu <zhangtianyu5177@sina.com>
2019-09-28 10:36:42 +0800

    init

--- day05/1.login&register/server.js
+++ day05/1.login&register/server.js
@@ -1,5 +1,9 @@
 //引入express框架
 let express = require('express')
+//引入db模块
+let db = require('./db')
+//引入usersModel模型对象，用于增删改查
+let usersModel = require('./models/usersModel')
 //实例应用对象
 let app = express()
 //对客户端隐藏服务器内部实现
@@ -7,30 +11,60 @@ app.disable('x-powered-by')
 //使用中间件解析post请求的urlencoded编码参数
 app.use(express.urlencoded({extended:true}))
 
-//注册的路由
-app.post('/register',(req,res)=>{
-  //1.获取用户输入的信息
-  let {email,nick_name,pwd,re_pwd} = req.body
-  //2.定义正则规则
-  let emailReg = /^[a-zA-Z0-9_]{2,20}@[a-zA-Z0-9_]{2,16}\.com$/
-  let nickNameReg = /[\u4e00-\u9fa5]/gm
-  let pwdReg = /^[a-zA-Z0-9_@#$%]{6,20}$/
-  //3.使用正则校验
-  if(!emailReg.test(email)){
-    res.send('邮箱输入不合法，格式应为：用户名@主机名.com的形式，用户名2-20，主机名2-16')
-  }else if(!nickNameReg.test(nick_name)){
-    res.send('昵称不合法，应为中文')
-  }else if(!pwdReg.test(pwd)){
-    res.send('密码不合法，包含大小写字母，和部分特殊字符，且长度为6-20')
-  }else if(re_pwd !== pwd){
-    res.send('两次输入密码不一致')
-  }else{
-    res.send('校验通过了')
-  }
+//等待数据库连接成功
+db.then(()=>{
+  
+  //展示登录界面的路由 --- UI路由
+  app.get('/login',(req,res)=>{
+    res.sendFile(__dirname+'/public/login.html')
+  })
 
-  //3.写入数据库之前，进行检查邮箱是否已经注册过了
-  //若已经注册了，驳回
-  //若未注册，写入数据库
+  //展示注册界面的路由 ---- UI路由
+  app.get('/register',(req,res)=>{
+    res.sendFile(__dirname+'/public/register.html')
+  })
+
+  //注册的路由 ----- 业务路由
+  app.post('/register',async(req,res)=>{
+    //1.获取用户输入的信息
+    let {email,nick_name,pwd,re_pwd} = req.body
+    //2.定义正则规则
+    let emailReg = /^[a-zA-Z0-9_]{2,20}@[a-zA-Z0-9_]{2,16}\.com$/
+    let nickNameReg = /[\u4e00-\u9fa5]/gm
+    let pwdReg = /^[a-zA-Z0-9_@#$%]{6,20}$/
+    //3.使用正则校验
+    if(!emailReg.test(email)){
+      res.send('邮箱输入不合法，格式应为：用户名@主机名.com的形式，用户名2-20，主机名2-16')
+    }else if(!nickNameReg.test(nick_name)){
+      res.send('昵称不合法，应为中文')
+    }else if(!pwdReg.test(pwd)){
+      res.send('密码不合法，包含大小写字母，和部分特殊字符，且长度为6-20')
+    }else if(re_pwd !== pwd){
+      res.send('两次输入密码不一致')
+    }else{
+      try{
+        //4.写入数据库之前，进行检查邮箱是否已经注册过了
+        let findResult = await usersModel.findOne({email:email})
+        if(findResult){
+          //能进入此判断，意味着有人已经注册过了，驳回
+          res.send(`${email}邮箱已经注册过，请更换邮箱`)
+        }else{
+          await usersModel.create({email:email,nick_name:nick_name,pwd:pwd})
+          console.log(`邮箱为${email}的用户注册成功了`)
+          res.send('恭喜，注册成功！')
+        }
+      }
+      catch(err){
+        //1.引入报警模块
+        //2.引入计数模块
+        console.log(err)
+        res.send('网络不稳定稍后重试！')
+      }
+    }
+  })
+  
+}).catch((err)=>{
+    console.log(err)
 })
 
 //绑定端口监听
