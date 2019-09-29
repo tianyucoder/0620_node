9165387cf4513198509ef3e0d3c7fee5c752ce03
zhangtianyu <zhangtianyu5177@sina.com>
2019-09-29 17:01:28 +0800

    init

--- day07(Ajax重要)/server.js
+++ day07(Ajax重要)/server.js
@@ -22,7 +22,7 @@ app.post('/ajax_post',function (request,response) {
 app.listen(3000,function (err) {
   if(!err){
     console.log('练习ajax请求的服务器启动成功了')
-    console.log('【禁止使用编译打开网页！！！！！！！！！】')
+    console.log('【为了避免跨域问题，禁止使用编译打开网页！！！！！！！！！】')
     console.log('测试原生Ajax-GET请求的页面是：http://localhost:3000/ajax_get.html')
     console.log('测试原生Ajax-POST请求的页面是：http://localhost:3000/ajax_post.html')
   }else{
