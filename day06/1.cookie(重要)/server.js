991cc2d0d8ac0e67720104b60b108476b4d7d84e
zhangtianyu <zhangtianyu5177@sina.com>
2019-09-29 10:41:43 +0800

    init

--- day06/1.cookie(重要)/server.js
+++ day06/1.cookie(重要)/server.js
@@ -1,6 +1,8 @@
 let express= require('express')
+let cookieParser = require('cookie-parser')
 
 let app = express()
+app.use(cookieParser())
 
 //demo路由不对进行任何cookie的操作
 app.get('/demo',(request,response)=>{
@@ -9,8 +11,34 @@ app.get('/demo',(request,response)=>{
 
 //当访问test1路由的时候，会给客户端种下一个cookie
 app.get('/test1',(request,response)=>{
-  response.cookie('demo','0620')
+  //在express中给客户端“种”下一个cookie，不用借助任何的第三方库。
+
+  //给客户端种下一个会话cookie，关闭浏览器，cookie立刻消失
+  //response.cookie('demo','你好0620')
+
+  //给客户端种下一个持久化cookie，到了过期时间，或者用户清空cookie，cookie立刻消失
+  response.cookie('demo','0620',{maxAge:1000 * 30})
+  response.cookie('demo1','hello',{maxAge:1000 * 30})
+
   response.send('我给你“种”下了一个cookie，你赶紧看一看')
+})
+
+//test2用于获取客户端携带过来的cookie
+app.get('/test2',(request,response)=>{
+  //在express中读取cookie，要借助一个中间件，名为：cookie-parser
+  let {demo} = request.cookies
+  console.log(demo)
+  response.send('我读取出来了你携带过来的cookie，你看看吧')
+})
+
+//test3用户通知浏览器删除一个cookie
+app.get('/test3',(request,response)=>{
+  //第一种删除cookie的方式
+  //response.clearCookie('demo')
+
+  //第二种删除cookie的方式
+  response.cookie('demo','',{maxAge:0})
+  response.send('我已经命令了你删除了指定的cookie')
 })
 
 app.listen(3000,function (err) {
---
+++ day06/2.login&register_cookie/server.js
@@ -0,0 +1,40 @@
+//引入express框架
+let express = require('express')
+//引入db模块
+let db = require('./db')
+//引入UI路由器，用于展示页面
+let uiRouter = require('./router/uiRouter')
+//引入登录、注册的业务路由
+let loginRegisterRouter = require('./router/loginRegisterRouter')
+//实例应用对象
+let app = express()
+//对客户端隐藏服务器内部实现
+app.disable('x-powered-by')
+//使用中间件解析post请求的urlencoded编码参数
+app.use(express.urlencoded({extended:true}))
+//设置模板引擎
+app.set('view engine','ejs')
+//设置模板目录
+app.set('views','./views')
+
+//等待数据库连接成功
+db.then(()=>{
+
+  //使用uiRouter
+  app.use(uiRouter)
+
+  //使用登录、注册路由器
+  app.use(loginRegisterRouter)
+
+}).catch((err)=>{
+    console.log(err)
+})
+
+//绑定端口监听
+app.listen(3000,(err)=> {
+  if(!err){
+    console.log('服务器启动成功了')
+  }else{
+    console.log(err)
+  }
+})
