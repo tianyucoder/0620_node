7575c606373f43e118708fbbc5dab542245fadd1
zhangtianyu <zhangtianyu5177@sina.com>
2019-09-30 15:01:09 +0800

    init

---
+++ day07(Ajax重要)/3.jquery封装的Ajax/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(__dirname+'/public'))
+app.use(express.urlencoded({extended:true}))
+
+
+//此路由响应get请求
+app.get('/ajax_get',function (request,response) {
+  console.log('有人给我发GET请求了')
+  console.log(request.query)
+  response.send(`你发来的是ajax-get请求，我接收到了`)
+})
+
+app.post('/ajax_post',function (request,response) {
+  console.log('有人给我发POST请求了')
+  console.log(request.body)
+  response.send('你发来的是ajax-post请求，我接收到了')
+})
+
+app.listen(3000,function (err) {
+  if(!err){
+    console.log('练习ajax请求的服务器启动成功了')
+    console.log('【为了避免跨域问题，禁止使用编译打开网页！！！！！！！！！】')
+    console.log('测试jquery封装的Ajax页面是：http://localhost:3000/ajax_jquery.html')
+  }else{
+    console.log(err)
+  }
+})
+
+
+
