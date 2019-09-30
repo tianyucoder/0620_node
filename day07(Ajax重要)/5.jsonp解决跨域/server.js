4dff02797b9f50b6bbe63f21cbf1122e6a041306
zhangtianyu <zhangtianyu5177@sina.com>
2019-09-30 16:03:12 +0800

    init

---
+++ day07(Ajax重要)/5.jsonp解决跨域/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.urlencoded({extended:true}))
+
+//此路由响应get请求
+app.get('/test',function (request,response) {
+  let {callback} = request.query
+  let data = [{name:'kobe',age:18},{name:'wade',age:19},{name:'peiqi',age:20}]
+  response.send(`${callback}(${JSON.stringify(data)})`)
+})
+
+app.listen(3000,function (err) {
+  if(!err){
+    console.log('【为了制造跨域问题，必须使用编译打开网页！！！！！！！！！】')
+  }else{
+    console.log(err)
+  }
+})
+
+
+
